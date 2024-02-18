be25de02ee4f4b3b3f8b01e34e061db004e8c611
zerobitone <pst@zerobit.one>
2024-02-18 09:17:30 +0100

    config local database connection

--- config/app.php
+++ config/app.php
@@ -287,7 +287,11 @@ return [
             /*
              * For MariaDB/MySQL the internal default changed from utf8 to utf8mb4, aka full utf-8 support, in CakePHP 3.6
              */
-            //'encoding' => 'utf8mb4',
+            'username' => 'db',
+            'password' => 'db',
+            'database' => 'db',
+            'encoding' => 'utf8mb4',
+
 
             /*
              * If your MySQL server is configured with `skip-character-set-client-handshake`
